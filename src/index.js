c5fc6d103767b62e4d4248cb0482d63012b62125
Igboanugwo Collins <munisco12@gmail.com>
2019-06-21 09:56:20 +0100

    created my store and reducer

--- src/index.js
+++ src/index.js
@@ -4,14 +4,18 @@ import './sass/main.scss';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './store/reducers/reducer'
 
+const store = createStore(reducer)
 const app = (
     <BrowserRouter>
         <App/>
     </BrowserRouter>
     )
 
-ReactDOM.render(app, document.getElementById('root'));
+ReactDOM.render(<Provider store = {store}>{app}</Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/store/reducers/reducer.js
@@ -0,0 +1,9 @@
+const initialState = {
+    ingredients: 0
+}
+
+const reducer = (state = initialState, action) =>{
+    return state
+}
+
+export default reducer
