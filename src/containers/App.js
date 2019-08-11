8ec88128ade659f95ff30469868be878d77b81a8
Igboanugwo Collins <niscolinx@localhost.localdomain>
2019-08-11 04:01:18 +0100

    From linux

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,11 +3,7 @@ import '../App.scss'
 import Layout from "../components/Layout/Layout"
 import Aux from "../components/hoc/HigherOrder"
 import BurgerBuilder from './BurgerBuilder'
-<<<<<<< HEAD
-import {Route, Switch} from 'react-router-dom'
-=======
 import { Route, Switch } from 'react-router-dom'
->>>>>>> 611a6124cd4ef3cb3c7791d08b17fa91f05b95fc
 import Checkout from '../containers/Checkout'
 import Orders from './Orders'
 
@@ -16,15 +12,6 @@ class App extends Component {
   render() {
     return (
       <Aux>
-<<<<<<< HEAD
-      <Layout>
-      <Switch>
-      <Route path = '/Checkout' component = {Checkout}/>
-      <Route path = '/Orders' component = {Orders}/>
-      <Route path = '/' exact component = {BurgerBuilder}/>
-      </Switch>
-      </Layout>
-=======
         <Layout>
           <Switch>
             <Route path='/Checkout' component={Checkout} />
@@ -32,7 +19,6 @@ class App extends Component {
             <Route path='/' exact component={BurgerBuilder} />
           </Switch>
         </Layout>
->>>>>>> 611a6124cd4ef3cb3c7791d08b17fa91f05b95fc
       </Aux>
     );
   }
--- src/containers/BurgerBuilder.js
+++ src/containers/BurgerBuilder.js
@@ -1,9 +1,5 @@
 import React, { Component } from "react";
-<<<<<<< HEAD
-import {connect} from 'react-redux'
-=======
 import { connect } from 'react-redux'
->>>>>>> 611a6124cd4ef3cb3c7791d08b17fa91f05b95fc
 
 import Burger from '../components/Burger/Burger'
 import BuildControls from "../components/Burger/BuildControl/BuildControls"
@@ -13,23 +9,12 @@ import withErrorHandler from "../components/hoc/withErrorHandler"
 import axios from "../axios"
 import Spinner from "../components/Layout/spinner"
 import OrderSummary from "../components/Modal/OrderSummary"
-<<<<<<< HEAD
-import * as actionTypes from '../store/actions'
-=======
 import * as burgerActions from '../store/actions/burgerIndex'
->>>>>>> 611a6124cd4ef3cb3c7791d08b17fa91f05b95fc
 
 
 class BurgerBuilder extends Component {
   state = {
-<<<<<<< HEAD
-    purchasable: false,
     orderSummary: false,
-    loading: false,
-    error: false
-=======
-    orderSummary: false,
->>>>>>> 611a6124cd4ef3cb3c7791d08b17fa91f05b95fc
   };
 
   orderSummary = () => {
@@ -45,23 +30,6 @@ class BurgerBuilder extends Component {
   };
 
   orderContinue = () => {
-<<<<<<< HEAD
-    
-    this.props.history.push('/Checkout');
-
-  //componentDidMount() {
-    // axios
-    //   .get("https://my-react-burger-1ce01.firebaseio.com/ingredients.json")
-    //   .then(res => {
-    //     this.setState({
-    //       ingredients: res.data
-    //     });
-    //   })
-    //   .catch(error => {
-    //     this.setState({ error: true });
-    //   });
-  }
-=======
 
     this.props.history.push('/Checkout');
   }
@@ -69,58 +37,34 @@ class BurgerBuilder extends Component {
     this.props.onInitIngredients()
   };
 
->>>>>>> 611a6124cd4ef3cb3c7791d08b17fa91f05b95fc
   orderbtn = newIng => {
 
     const orderIng = Object.values(newIng).reduce((orderIng, el) => {
       return orderIng + el;
     }, 0);
 
-<<<<<<< HEAD
-    
-    return orderIng > 0
-   };
-=======
 
     return orderIng > 0
   };
->>>>>>> 611a6124cd4ef3cb3c7791d08b17fa91f05b95fc
 
 
   render() {
     let ingDisabled = { ...this.props.ingredients };
-<<<<<<< HEAD
-    
-=======
-
->>>>>>> 611a6124cd4ef3cb3c7791d08b17fa91f05b95fc
     for (let ing in ingDisabled) {
       ingDisabled[ing] = ingDisabled[ing] <= 0;
     }
     let totalSummary = null;
 
-<<<<<<< HEAD
-    let burger = this.state.error ? (
-      <p style ={{
-=======
     let burger = this.props.error ? (
       <p style={{
->>>>>>> 611a6124cd4ef3cb3c7791d08b17fa91f05b95fc
         textAlign: 'center',
         fontSize: '1.4rem'
       }}>Ingredients can't be loaded. Please check your internet connection</p>
     ) : (
-<<<<<<< HEAD
-      <div style={{ textAlign: "center" }}>
-        <Spinner />
-      </div>
-    );
-=======
         <div style={{ textAlign: "center" }}>
           <Spinner />
         </div>
       );
->>>>>>> 611a6124cd4ef3cb3c7791d08b17fa91f05b95fc
     if (this.props.ingredients) {
       burger = (
         <Aux>
@@ -146,24 +90,13 @@ class BurgerBuilder extends Component {
         />
       );
     }
-<<<<<<< HEAD
-    if (this.state.loading) {
-      totalSummary = <Spinner />;
-    }
-=======
->>>>>>> 611a6124cd4ef3cb3c7791d08b17fa91f05b95fc
 
     return (
       <div>
         <Modal
           orderSummary={this.state.orderSummary}
-<<<<<<< HEAD
-          clicked={this.orderCancel}
-        >
-=======
           clicked={this.orderCancel}>
 
->>>>>>> 611a6124cd4ef3cb3c7791d08b17fa91f05b95fc
           {totalSummary}
         </Modal>
         {burger}
@@ -174,27 +107,16 @@ class BurgerBuilder extends Component {
 
 const mapStateToProps = state => {
   return {
-<<<<<<< HEAD
-    ingredients: state.ingredients,
-    totalPrice: state.totalPrice,
-    purchasable: state.purchasable
-=======
     ingredients: state.burger.ingredients,
     totalPrice: state.burger.totalPrice,
     error: state.burger.error
->>>>>>> 611a6124cd4ef3cb3c7791d08b17fa91f05b95fc
   }
 }
 const mapDispatchToProps = dispatch => {
   return {
-<<<<<<< HEAD
-    onAddIngredient: (ingName) => dispatch({type: actionTypes.ADD_INGREDIENT, ingredientName:ingName}),
-    onRemoveIngredient: (ingName) => dispatch({type: actionTypes.REMOVE_INGREDIENT, ingredientName: ingName})
-=======
     onAddIngredient: (ingName) => dispatch(burgerActions.addIngredient(ingName)),
     onRemoveIngredient: (ingName) => dispatch(burgerActions.removeIngredient(ingName)),
     onInitIngredients: () => dispatch(burgerActions.setIngredients())
->>>>>>> 611a6124cd4ef3cb3c7791d08b17fa91f05b95fc
   }
 }
 
