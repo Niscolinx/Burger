1abaca97282c01f867c9d1e463dc4eeade295880
Niscolinx <munisco12@gmail.com>
2019-09-17 16:06:52 +0100

    Updates

---
+++ precache-manifest.3d8a750f8c10bb802da68995c3ff8b2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "e4f707eb8257d40b61ad",
+    "url": "/burger/static/js/runtime~main.e4f707eb.js"
+  },
+  {
+    "revision": "5cda29defe65437d236f",
+    "url": "/burger/static/js/main.5cda29de.chunk.js"
+  },
+  {
+    "revision": "18990fa449a5ae60cc6a",
+    "url": "/burger/static/js/1.18990fa4.chunk.js"
+  },
+  {
+    "revision": "5cda29defe65437d236f",
+    "url": "/burger/static/css/main.33abb224.chunk.css"
+  },
+  {
+    "revision": "f783d7d93c84fd5d6520f0a63112e0dd",
+    "url": "/burger/index.html"
+  }
+];
